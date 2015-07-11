7f5c01d21ac4348c2e4e127082d3b06d2f999259
Margaret Sy <margaretsy1016@gmail.com>
2015-07-11 00:02:37 -0700

    2-way naive ncc alignment works

--- colorize.py
+++ colorize.py
@@ -6,8 +6,10 @@
 import sys
 import numpy as np
 from skimage import io, util
+from sklearn.metrics.pairwise import pairwise_distances
+from itertools import product
 import matplotlib.pyplot as plt
-from scipy.misc import imread #, imfilter, imresize, imrotate, imsave, imshow
+from scipy.misc import imread, imsave #, imfilter, imresize, imrotate, imshow
 
 
 def colorize(bw_file):
@@ -23,23 +25,33 @@ def colorize(bw_file):
 
     full_img = imread(bw_file)
     height = len(full_img)/3
-    b, g, r = np.split(full_img, [height, height*2])
-    aligned_g = util.img_as_float(align_imgs(g, b))
-    aligned_r = util.img_as_float(align_imgs(r, b))
-    b = util.img_as_float(b)
+    b, g, r = map(util.img_as_float, np.split(full_img, [height, height*2]))
+    r = align_imgs(r, g)
+    print b.shape
+    print g.shape
+    print r.shape
+    aligned_r, aligned_b = align_naive(r,b)
+    aligned_g, aligned_b = align_naive(g,b)
+    # aligned_g = util.crop(aligned_g, ((0,3),(0,1)))
+    # aligned_b = util.crop(aligned_b, ((0,3),(0,1)))
+    # aligned_r = align_imgs(r,b)
+    # aligned_g = align_imgs(g,b)
+    # aligned_b = b
     print aligned_r.shape
     print aligned_g.shape
-    print b.shape
-    return np.dstack((aligned_r,aligned_g,b))
+    print aligned_b.shape
+    return np.dstack((aligned_g,aligned_g,aligned_b))
 
 
 def align_imgs(img1, img2):
     """
+    more like make img1 same height as img2
     params img1 and img2 are ndarrays
-    returns img1 aligned to img2? not sure what this is supposed to
-    do but for now it just returns img1 arbitrarily cropped to match
+    returns img1 cropped on the bottom to match
     the shape of img2. Only could differ on height, since the original
     images were concatenated in a vertical stack.
+
+    ok this is
     """
     aligned = None
     if img1.shape == img2.shape:
@@ -50,9 +62,78 @@ def align_imgs(img1, img2):
         if difference > 0:
             aligned = util.crop(img1, ((0,difference), (0,0)))
         else:
-            aligned = np.append(img1, np.zeros(shape=(-difference, img1.shape[1])), axis=0)
+            # aligned = np.append(img1, np.zeros(shape=(-difference, img1.shape[1])), axis=0)
+            aligned = util.pad(img1, ((0,difference), (0,0)), mode='edge')
         return aligned
 
+
+def align_naive(img1, img2, window=15, scorer='ncc'):
+    """
+    find optimal alignment if of img1 wrt img2
+    :param img1: ndarray
+    :param img2: ndarray
+    :return:
+    """
+    def ncc(img1,img2):
+        """
+        normalized cross-correlation of images
+        dot product of normalized vectors
+
+        :param img1:
+        :param img2:
+        :return:
+        """
+        normed1 = normalize(img1).flatten()
+        normed2 = normalize(img2).flatten()
+        return normed1.dot(normed2)
+
+    metrics = {'ncc':ncc}
+
+    distance = metrics[scorer]
+    best = 0
+    aligned1 = None
+    aligned2 = None
+    for displacement in product(xrange(window), xrange(window)):
+        # print displacement
+        disp1, disp2 = displace(img1, img2, displacement)
+        score = distance(disp1, disp2)
+        # score = pairwise_distances(disp1, disp2, metric='cosine')
+        if score > best:
+            print "new score", score, displacement
+            best = distance(disp1, disp2)
+            aligned1, aligned2 = disp1, disp2
+    print best
+    return aligned1, aligned2
+
+
+def displace(img1, img2, d):
+    """
+    :param img1:
+    :param img2: shapes should be equal
+    :param displacement: tuple (x,y) to shift img2 wrt img1 where positive
+     numbers indicate down/right and negative displacement is up/left
+    :return img1, img2: cropped/displaced
+    examples
+    img1    img2
+    1 2 3   a b c
+    4 5 6   d e f
+    7 8 9   g h i
+
+    (1,0) returns     (-1, 1) returns
+    1 2 3   d e f       4 5    b c
+    4 5 6   g h i       7 8    e f
+    """
+    x,y = d
+    disp = lambda x: (0,x) if x > 0 else (-x,0)
+    disp1 = util.crop(img1, (disp(x), disp(y)))
+    disp2 = util.crop(img2, (disp(-x), disp(-y)))
+    return disp1, disp2
+
+
+def normalize(a):
+    # assumes floats
+    row_sums = a.sum(axis=1)
+    return a / row_sums[:, np.newaxis]
 
 
 def show_ndarr_as_img(img):
@@ -77,12 +158,17 @@ def show_ndarr_as_img(img):
 if __name__ == "__main__":
     try:
         bw_filename = sys.argv[1]
-    except Error as e:
+        color_filename = sys.argv[2]
+    except Exception as e:
         print "Usage:\n$ python colorize.py imagename.ext"
         print e
 
     color_img = colorize(bw_filename)
     print color_img.shape
+
     show_ndarr_as_img(color_img)
 
+    if len(sys.argv) > 2:
+        color_filename = sys.argv[2]
+        io.imsave(color_filename, color_img)
 
